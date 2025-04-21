b5828c52dc4d4e7b9c01ac3c8878b145b353e689
Roman Baranovskii <rom8614@gmail.com>
2025-04-21 14:25:17 +0300

    swiper btns + more photos

--- src/components/ui/SlickSlider/SlickSlider.jsx
+++ src/components/ui/SlickSlider/SlickSlider.jsx
@@ -16,11 +16,12 @@ const DesktopLeftLoopSwiper = () => {
     const [activeIndex, setActiveIndex] = useState(0);
 
     const slideData = [
-        { id: 0, content: 'Активный 1', image: '/Anton.png', name: `${t("about.Anton")}`, role: "Product Manager"},
-        { id: 1, content: 'Слайд 2', image: '/Eugene.png', name: `${t("about.Eugene")}`, role: "Business analyst"},
-        { id: 2, content: 'Слайд 3', image: '/Ivan.png', name:  `${t("about.Ivan")}`, role: "UX/UI designer"},
-        { id: 3, content: 'Слайд 4', image: '/Nickolay.png', name: `${t("about.Nickolay")}` , role: "Product Manager"},
-        { id: 4, content: 'Слайд 5', image: '/Roman.png', name: `${t("about.Roman")}`, role: "Full-stack developer"},
+        { id: 0, content: 'Слайд 1', image: '/Anton.png', name: `${t("about.Anton")}`, role: "Product Manager"},
+        { id: 1, content: 'Слайд 2', image: '/Roman.png', name: `${t("about.Roman")}`, role: "Full-stack Developer"},
+        { id: 2, content: 'Слайд 3', image: '/Ivan.png', name:  `${t("about.Ivan")}`, role: "UX/UI Designer"},
+        { id: 3, content: 'Слайд 4', image: '/Valeri.png', name: `${t("about.Valeri")}`, role: "QA Engineer"},
+        { id: 4, content: 'Слайд 5', image: '/Nickolay.png', name: `${t("about.Nickolay")}` , role: "Product Manager"},
+        { id: 5, content: 'Слайд 6', image: '/Mironov.png', name: `${t("about.Valerii")}`, role: "Content Manager"},
     ];
 
     const handleSwiperInit = (swiperInstance) => {
@@ -32,50 +33,76 @@ const DesktopLeftLoopSwiper = () => {
         setActiveIndex(swiperInstance.realIndex);
     };
 
+    const handleSlidePrev = () => {
+        if (swiperRef.current) {
+            swiperRef.current.slidePrev();
+        }
+    };
+
+    const handleSlideNext = () => {
+        if (swiperRef.current) {
+            swiperRef.current.slideNext();
+        }
+    };
+
     return (
-        <div className={styles.swiperContainer}>
-            <Swiper
-                onSwiper={handleSwiperInit}
-                onSlideChange={handleSlideChange}
-                modules={[A11y, Autoplay]}
-                spaceBetween={20}
-                grabCursor={true}
-                loop={true}
-                slidesPerView={1}
-                centeredSlides={true}
-                navigation={false}
-                autoplay={{
-                    delay: 4000,
-                    disableOnInteraction: false,
-                    pauseOnMouseEnter: true,
-                }}
-                breakpoints={{
-                    720: {
-                        slidesPerView: 'auto',
-                        centeredSlides: false,
-                        spaceBetween: 0,
-                    },
-                    1024: {
-                        slidesPerView: 'auto',
-                        centeredSlides: false,
-                        spaceBetween: 25,
-                    }
-                }}
-                className={styles.mySwiper}
-            >
-                {slideData.map((slide) => (
-                    <SwiperSlide key={slide.id} className={styles.swiperSlide}>
-                        <div className={activeIndex === slide.id ? styles.activeSlideStyle : styles.baseSlideStyle}>
-                            <Image className={styles.slideImage} src={slide.image} alt={''} width={170} height={230} />
-                            <div className={styles.slideDesc}>
-                                {slide.name}<br/>
-                                {slide.role}
+        <>
+            <div className={styles.swiperContainer}>
+                <Swiper
+                    onSwiper={handleSwiperInit}
+                    onSlideChange={handleSlideChange}
+                    modules={[A11y, Autoplay]}
+                    spaceBetween={20}
+                    grabCursor={true}
+                    loop={true}
+                    slidesPerView={1}
+                    centeredSlides={true}
+                    navigation={false}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                        pauseOnMouseEnter: true,
+                    }}
+                    breakpoints={{
+                        720: {
+                            slidesPerView: 'auto',
+                            centeredSlides: false,
+                            spaceBetween: 0,
+                        },
+                        1024: {
+                            slidesPerView: 'auto',
+                            centeredSlides: false,
+                            spaceBetween: 25,
+                        }
+                    }}
+                    className={styles.mySwiper}
+                >
+                    {slideData.map((slide) => (
+                        <SwiperSlide key={slide.id} className={styles.swiperSlide}>
+                            <div className={activeIndex === slide.id ? styles.activeSlideStyle : styles.baseSlideStyle}>
+                                <Image className={styles.slideImage} src={slide.image} alt={''} width={170} height={230} />
+                                <div className={styles.slideDesc}>
+                                    {slide.name}<br/>
+                                    {slide.role}
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            <div className={styles.swiperBtnSet}>
+                <div className={styles.swiperBtnPrev} onClick={handleSlidePrev}>
+                    <svg width="20" height="20" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 12L20 0.452994L20 23.547L0 12Z" fill="black"/>
+                    </svg>
+                </div>
+                <div className={styles.swiperBtnNext} onClick={handleSlideNext}>
+                    <svg width="20" height="20" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20 12L0 0.452994L0 23.547L20 12Z" fill="black"/>
+                    </svg>
+                </div>
+            </div>
+        </>
     );
 };
 
